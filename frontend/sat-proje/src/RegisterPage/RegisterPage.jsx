8f15107c57d76e36c07bd55ccd00cdd4549d0251
AltanTuran <150243973+AltanTuran@users.noreply.github.com>
2026-04-02 19:45:22 +0300

    register auth

--- frontend/sat-proje/src/RegisterPage/RegisterPage.jsx
+++ frontend/sat-proje/src/RegisterPage/RegisterPage.jsx
@@ -23,7 +23,7 @@ function RegisterPage() {
   }
 
   try {
-    const response = await fetch(`${API_URL}/api/register`, {
+    const response = await fetch(`${API_URL}/api/auth/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
